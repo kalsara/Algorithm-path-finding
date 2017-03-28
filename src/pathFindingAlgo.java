8f8e867c2cf877897e4ab82b73d265448447793e
kalsara <kalsaramagamage@gmail.com>
2017-03-28 18:18:10 +0530

    make the square grid

--- src/pathFindingAlgo.java
+++ src/pathFindingAlgo.java
@@ -2,7 +2,18 @@
 public class pathFindingAlgo{
 
 
-
+/make grid
+    public static void show(boolean[][] a, boolean which) {
+        int N = a.length;
+        StdDraw.setXscale(-1, N);
+        StdDraw.setYscale(-1, N);
+        StdDraw.setPenColor(StdDraw.BLACK);
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (a[i][j] == which)
+                    StdDraw.square(j, N - i - 1, .5);
+                else StdDraw.filledSquare(j, N - i - 1, .5);
+    }
 
 
 }
