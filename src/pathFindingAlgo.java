dc0f754e17ff6e4b79b614e4e30f52e6dca2c72d
kalsara <kalsaramagamage@gmail.com>
2017-03-29 18:31:01 +0530

    show the back track path

--- src/pathFindingAlgo.java
+++ src/pathFindingAlgo.java
@@ -23,6 +23,56 @@ public class pathFindingAlgo{
                 a[i][j] = StdRandom.bernoulli(p);
         return a;
     }
+// draw the N-by-N boolean matrix to standard draw, including the points A (x1, y1) and B (x2,y2) to be marked by a circle
+    //draw particular path by a line
+    public static void show(boolean[][] a, boolean which, int x1, int y1, int x2, int y2,ArrayList<Node> backTrackPath) {
+        int N = a.length;
+        int s = backTrackPath.size();
+        int count = 0;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (a[i][j] == which)
+                    if ((i == x1 && j == y1) || (i == x2 && j == y2)) {
+                        StdDraw.setPenColor(Color.GREEN);
+                        StdDraw.circle(j, N - i - 1, .4);
+
+                    }
+
+        int i = 0;
+        int prex = 0;
+        int prey = 0;
+        Collections.reverse(backTrackPath);
+
+        for (Node Node : backTrackPath) {
+           /* if(s-count==1){
+                return;
+            }*/
+            count++;
+
+           try {
+                TimeUnit.MILLISECONDS.sleep(100);
+
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            StdDraw.setPenColor(Color.RED);
+            if (i == 0) {
+
+                i++;
+                prex = Node.x;
+                prey = Node.y;
+            } else {
+
+                StdDraw.setPenRadius(0.01);
+                StdDraw.line(prey, N - prex - 1, Node.y, N - Node.x - 1);
+                prey = Node.y;
+                prex = Node.x;
+
+
+            }
+        }
+    }
 
 
 }
