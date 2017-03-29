e21f57b57c9bc83c702a48f5ea1e2b206d45ba23
kalsara <kalsaramagamage@gmail.com>
2017-03-29 18:27:40 +0530

    generate random perculat

--- src/pathFindingAlgo.java
+++ src/pathFindingAlgo.java
@@ -14,6 +14,15 @@ public class pathFindingAlgo{
                     StdDraw.square(j, N - i - 1, .5);
                 else StdDraw.filledSquare(j, N - i - 1, .5);
     }
+// return a random N-by-N boolean matrix, where each entry is
+    // true with probability p
+    public static boolean[][] random(int N, double p) {
+        boolean[][] a = new boolean[N][N];
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                a[i][j] = StdRandom.bernoulli(p);
+        return a;
+    }
 
 
 }
